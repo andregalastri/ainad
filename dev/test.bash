830a0f4ad59015078902200732389a908d5bba21
andregalastri <contato@andregalastri.com.br>
2022-11-18 02:33:06 +0000

    Unnecessary file

--- dev/test.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo sed -i \
-        -e "s/^;extension=intl/extension=intl/" \
-        -e "s/^;extension=pdo_mysql/extension=pdo_mysql/" \
-        -e "s/^;extension=pdo_sqlite/extension=pdo_sqlite/" \
-        "/etc/php/php.ini";
