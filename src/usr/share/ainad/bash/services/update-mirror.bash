1022851628c592314a1ceec44800213e93b8b320
andregalastri <contato@andregalastri.com.br>
2022-12-25 21:39:33 +0000

    Trying to fix problems with mirrors

--- src/usr/share/ainad/bash/services/update-mirror.bash
+++ src/usr/share/ainad/bash/services/update-mirror.bash
@@ -5,4 +5,7 @@
 # mirrors.
 # ------
 
-curl -s "https://archlinux.org/mirrorlist/?country=BR&protocol=https&protocol=http&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - | sudo tee '/etc/pacman.d/mirrorlist';
+sleep 10;
+curl -s "https://archlinux.org/mirrorlist/?country=BR&protocol=https&protocol=http&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - | sudo tee '/etc/pacman.d/mirrorlist-updated';
+sudo cat '/etc/pacman.d/mirrorlist-updated' | sudo tee '/etc/pacman.d/mirrorlist';
+sudo rm '/etc/pacman.d/mirrorlist-updated';
